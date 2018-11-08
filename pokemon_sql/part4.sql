d7601e2fc7275406cf0c359fc3f9c66b16acfa52
Trinh Tong <trinhtong@Zipcoders-MBP-8.hsd1.pa.comcast.net>
2018-11-07 21:32:04 -0500

    Part 4 finished

--- pokemon_sql/part4.sql
+++ pokemon_sql/part4.sql
@@ -1,2 +1,22 @@
 # Pokemon Name	Trainer Name	Level	Primary Type	Secondary Type
 # Rank pokemon by strongest
+# In all of my pokemon adventures, I strived for the fastest pokemon with the best attack (sp or norm)
+# So I ranked them by level and then added the speed, sp atk and atk together!
+SELECT p.name as "Pokemon's Name", t.trainername as "Trainer's Name", pt.pokelevel as "Level",
+      ANY_VALUE(ty.name) as "Primary Type", ANY_VALUE(ty.name) as "Secondary Type"
+
+FROM pokemon.pokemon_trainer pt
+      JOIN pokemon.pokemons p
+        on p.id = pt.pokemon_id
+      JOIN pokemon.trainers t
+        on pt.trainerID = t.trainerID
+      JOIN pokemon.trainers tt
+        on pt.trainerID = tt.trainerID
+      JOIN pokemon.types ty
+        on ty.id = p.primary_type
+      JOIN pokemon.types ty2
+        on ty2.id = p.secondary_type
+GROUP BY p.name, t.trainername, pokelevel
+
+ORDER BY pt.pokelevel DESC,
+         SUM(pt.speed + pt.spatk + pt.attack) DESC;
